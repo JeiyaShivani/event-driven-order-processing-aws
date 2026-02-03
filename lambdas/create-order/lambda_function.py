691a93c60aef25e1e16adeb0e90bed7acec8a4d0
Jeiya Shivani <151517076+JeiyaShivani@users.noreply.github.com>
2026-02-03 18:42:33 +0530

    Implement idempotency for order creation process
    Add idempotency handling for order creation

--- lambdas/create-order/lambda_function.py
+++ lambdas/create-order/lambda_function.py
@@ -1,32 +1,63 @@
 import boto3
 import uuid
 from datetime import datetime
-import json
+from botocore.exceptions import ClientError
+from time import time
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('order')  # exact table name
+dynamodb = boto3.resource("dynamodb")
+orders_table = dynamodb.Table("order")
+idempotency_table = dynamodb.Table("OrderIdempotencyTable")
 
 def lambda_handler(event, context):
-    # raise Exception("INTENTIONAL FAILURE FOR TESTING SNS")
-    # Generate unique order ID
-    order_id = str(uuid.uuid4())
-    
-    # Build item
-    item = {
-        'order_id': order_id,
-        'status': 'CREATED',
-        'created_at': datetime.utcnow().isoformat()
-    }
-    
-    # Store in DynamoDB
-    table.put_item(Item=item)
-    
-    # Return response
-    return {
-        "order_id": order_id,
-        "status": "CREATED",
-        "product": event["product"],
-        "quantity": event["quantity"],
-        "price": event["price"],
-        "customer_id": event["customer_id"]
-    }
+    idempotency_key = event["idempotency_key"]
+
+    try:
+        # Create new order
+        order_id = str(uuid.uuid4())
+        ttl_seconds = int(time()) + (24 * 60 * 60)
+
+        idempotency_table.put_item(
+            Item={
+                "idempotency_key": idempotency_key,
+                "order_id": order_id,
+                "status": "CREATED",
+                "created_at": datetime.utcnow().isoformat(),
+                "ttl": ttl_seconds
+            },
+            ConditionExpression="attribute_not_exists(idempotency_key)"
+        )
+
+        # Store actual order
+        orders_table.put_item(
+            Item={
+                "order_id": order_id,
+                "product": event["product"],
+                "quantity": event["quantity"],
+                "price": event["price"],
+                "customer_id": event["customer_id"],
+                "status": "CREATED",
+                "created_at": datetime.utcnow().isoformat()
+            }
+        )
+
+        return {
+            "order_id": order_id,
+            "status": "CREATED",
+            "idempotent": False
+        }
+
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            # Duplicate request → fetch existing order
+            response = idempotency_table.get_item(
+                Key={"idempotency_key": idempotency_key}
+            )
+
+            return {
+                "order_id": response["Item"]["order_id"],
+                "status": response["Item"]["status"],
+                "idempotent": True
+            }
+
+        else:
+            raise
